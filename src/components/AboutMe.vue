d71abcceedb91697770ebd2a973ee2b9aa9865bc
alexcupit <alexcupit@outlook.com>
2023-01-10 18:13:37 +0000

    initial commit

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="hero min-h-screen"
+    style="background-image: url(https://placeimg.com/1000/800/arch)"
+  >
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md flex flex-col items-center">
+        <img
+          class="mask mask-circle w-160"
+          src="https://placeimg.com/160/160/arch"
+        />
+        <br />
+        <h1 class="mb-5 text-5xl font-bold">hello there</h1>
+        <p class="mb-5">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="bg-primary flex justify-between items-center">
+    <a class="btn btn-ghost normal-case text-lg text-neutral-content">
+      Alex Cupit
+    </a>
+    <nav>
+      <a class="mx-3">LinkedIn</a>
+      <a class="mx-3">GitHub</a>
+    </nav>
+  </header>
+</template>
+
+<style></style>

3c8250d341c4894cbb47ca34455e80129ab185b0
Alex Cupit <alexcupit@outlook.com>
2023-11-10 14:47:58 +0000

    fix: add figure into proj cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,18 +15,20 @@ defineProps({
       class="btn btn-ghost normal-case hover:bg-base-100 font-normal h-min text-left"
     >
       <div class="card w-80 md:w-96 bg-base-100 shadow-xl m-5 h-96">
-        <div class="relative">
-          <img
-            class="object-fill h-40 w-full blur-md absolute"
-            :src="cardImage"
-            :alt="projectName"
-          />
-          <img
-            class="object-scale-down h-40 w-full p-2 blur-none absolute"
-            :src="cardImage"
-            :alt="projectName"
-          />
-        </div>
+        <figure>
+          <div class="relative">
+            <img
+              class="object-fill h-40 w-full blur-md absolute"
+              :src="cardImage"
+              :alt="projectName"
+            />
+            <img
+              class="object-scale-down h-40 w-full p-2 blur-none"
+              :src="cardImage"
+              :alt="projectName"
+            />
+          </div>
+        </figure>
         <div class="card-body">
           <h3 class="card-title text-primary">{{ projectName }}</h3>
           <p>
